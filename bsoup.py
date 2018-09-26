f3649cdeec330f73ab8c697a9976e8b3f32ef418
Seth Patterson <seth.patterson@amplus-corp.com>
2018-09-26 14:12:29 -0600

    adding alpha and beta scraping to bsoup

--- bsoup.py
+++ bsoup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from bs4 import BeautifulSoup, Comment
+from bs4 import BeautifulSoup
 import urllib
 
 riskURL = "https://finance.yahoo.com/quote/SHSAX/risk"
@@ -8,24 +8,45 @@ page = urllib.urlopen(riskURL)
 content = page.read().decode('utf-8')
 soup = BeautifulSoup(content, 'html.parser')
 
-import pdb; pdb.set_trace()
+#import pdb; pdb.set_trace()
 
-
+std=[]
 std_span = next(x for x in soup.find_all('span') if x.text == "Standard Deviation")
 parent_div = std_span.parent
 for sibling in parent_div.next_siblings:
   for child in sibling.children:
      # do something
-     print(child.text)
+     #print(child.text)
+     std.append((child.text).encode("utf-8"))
 
 
-std = soup.find("span", {"data-reactid" : "121"}).text
+
 print std
+print '--------------------'
 
+alpha=[]
+alpha_span = next(x for x in soup.find_all('span') if x.text == "Alpha")
+parent_div = alpha_span.parent
+for sibling in parent_div.next_siblings:
+  for child in sibling.children:
+     # do something
+     #print(child.text)
+     alpha.append((child.text).encode("utf-8"))
 
-print float(soup.find(class_="W(57%) Mend(5px) Fl(end)").text)
-print float(soup.find("span",{"data-reactid":"124"}).text)
-print float(soup.find("span",{"data-reactid":"125"}).text)
+print alpha
+
+print '--------------------'
+
+beta=[]
+beta_span = next(x for x in soup.find_all('span') if x.text == "Beta")
+parent_div = beta_span.parent
+for sibling in parent_div.next_siblings:
+  for child in sibling.children:
+     # do something
+     #print(child.text)
+     beta.append((child.text).encode("utf-8"))
+
+print beta
 
 
 
