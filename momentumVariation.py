55c9f5d5f90c2df40a31031d0ea041aa20d6ff61
Seth Patterson <seth.patterson@amplus-corp.com>
2018-09-26 18:02:03 -0600

    adding first cut at momentum script

---
+++ momentumVariation.py
@@ -0,0 +1,94 @@
+#!/usr/bin/python
+
+import fix_yahoo_finance as yf
+yf.pdr_override()
+from pandas_datareader import data as pdr
+
+from bs4 import BeautifulSoup
+import requests
+#import pandas.io.data as web
+import datetime
+import dateutil
+import xlsxwriter
+import numpy
+import argparse
+import codecs
+import sys
+import time
+import urllib
+
+# email packages: http://kutuma.blogspot.com/2007/08/sending-emails-via-gmail-with-python.html
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email.MIMEText import MIMEText
+from email import Encoders
+import os
+# # # # # # # # # # # #
+import pprint as pp
+import matplotlib.pylab as plt
+
+
+def calcMomentum(stockTicker,startDate,totalDays):
+   # get 3 years ago
+   last_day = startDate + dateutil.relativedelta.relativedelta(days=-totalDays)
+   first_day  = last_day + dateutil.relativedelta.relativedelta(months=-36)
+
+   # request the data from yahoo 
+   stockData = yf.download(stockTicker, start=first_day, end=last_day)
+
+   momentumScore = {}
+
+   for n in range(totalDays):
+      calcDate = startDate + dateutil.relativedelta.relativedelta(days=-n)
+ 
+      month_1 = calcDate + dateutil.relativedelta.relativedelta(months=-1)
+      month_3 = calcDate + dateutil.relativedelta.relativedelta(months=-3)
+      month_6 = calcDate + dateutil.relativedelta.relativedelta(months=-6)
+      month_12 = calcDate + dateutil.relativedelta.relativedelta(months=-12)
+   
+   
+      stockDates = stockData.index
+      closeCurrDay = stockData.ix[stockDates[-1]]['Adj Close']
+   
+      ## get Year to Date performance
+      closestDate = min(stockDates,key=lambda date : abs(first_day-date))
+      closeFirstDay = stockData.ix[closestDate]['Adj Close']
+      perf_YTD =  (closeCurrDay - closeFirstDay)/closeFirstDay*100
+   
+      # Find closest day 1 month ago and calculate performance
+      closestDate = min(stockDates,key=lambda date : abs(month_1-date))
+      closeValue_month_1 = stockData.ix[closestDate]['Adj Close']
+      perf_1_month =  (closeCurrDay - closeValue_month_1)/closeValue_month_1*100
+   
+      # Find closest day 3 months ago and calculate performance
+      closestDate = min(stockDates,key=lambda date : abs(month_3-date))
+      closeValue_month_3 = stockData.ix[closestDate]['Adj Close']
+      perf_3_month =  (closeCurrDay - closeValue_month_3)/closeValue_month_3*100
+      # Find closest day 6 months ago and calculate performance
+      closestDate = min(stockDates,key=lambda date : abs(month_6-date))
+      closeValue_month_6 = stockData.ix[closestDate]['Adj Close']
+      perf_6_month =  (closeCurrDay - closeValue_month_6)/closeValue_month_6*100
+      # Find closest day 12 months ago and calculate performance
+      closestDate = min(stockDates,key=lambda date : abs(month_12-date))
+      closeValue_month_12 = stockData.ix[closestDate]['Adj Close']
+      perf_12_month =  (closeCurrDay - closeValue_month_12)/closeValue_month_12*100
+   
+      #%% Momentum
+      momentumScore[n] = perf_3_month + perf_6_month + perf_12_month
+
+   return momentumScore
+
+
+if __name__ == "__main__":
+    startDate = datetime.datetime.today()
+    stockTicker = 'DFCIX'
+    totalDays = 200
+    momentum = calcMomentum(stockTicker,startDate,totalDays)
+
+    lists = sorted(momentum.items())
+
+    x, y = zip(*lists) # unpack a list of pairs into two tuples
+    
+    plt.plot(x, y)
+    plt.show()
